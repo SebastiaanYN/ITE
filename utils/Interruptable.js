0d66d46d0bee4915828c949c8401195ae09f440c
SebastiaanYN <34006210+SebastiaanYN@users.noreply.github.com>
2018-04-16 20:58:55 +0200

    Add files via upload

---
+++ utils/Interruptable.js
@@ -0,0 +1,39 @@
+class Interruptable {
+  constructor() {
+    this.cancelled = false;
+  }
+
+  cancel() {
+    this.cancelled = true;
+    if (this._timer) {
+      clearTimeout(this._timer);
+      if (!this._reject) {
+        throw new Exception('_reject not set');
+      }
+      this._reject(1);
+    }
+  }
+
+  get promise() {
+    return this._promise;
+  }
+
+  sleep(duration) {
+    this.timerHolder = {};
+    return this._promise = new Promise((resolve, reject) => {
+      if (this.cancelled) {
+        return reject();
+      }
+      this._reject = reject;
+      this._timer = setTimeout(() => {
+        if (this.cancelled) {
+          reject();
+        } else {
+          resolve();
+        }
+      }, duration);
+    });
+  }
+
+}
+module.exports = Interruptable;
